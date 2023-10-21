a5c589d276e780a39982b426b3334b184e5ac2dc
Hamza Ouggadi <45943796+HamzaOuggadi@users.noreply.github.com>
2023-10-21 13:20:04 +0100

    Added first Stage and Scene

--- ClientApp/src/main/java/net/hamzaouggadi/ClientApp.java
+++ ClientApp/src/main/java/net/hamzaouggadi/ClientApp.java
@@ -2,12 +2,15 @@ package net.hamzaouggadi;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class ClientApp extends Application {
-
 
 
     public static void main(String[] args) {
@@ -16,11 +19,20 @@ public class ClientApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(label), 640, 480);
+        Image icon = new Image("icon.png");
+        Button button = new Button("Click !");
+        button.setOnAction(actionEvent -> {
+            button.setText("You Just Clicked Me !");
+        });
+        HBox hbox = new HBox(button);
+
+
+        Scene scene = new Scene(hbox, 640, 480, Color.LIGHTSKYBLUE);
+
         stage.setScene(scene);
+        stage.setTitle("ChatRoom");
+        stage.getIcons().add(icon);
+        stage.setResizable(false);
         stage.show();
     }
 }
